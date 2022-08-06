23b20bdada30dcedb289f407f9d8ae2e2cef2bad
陈乾 <qianpou@163.com>
2022-08-06 21:38:34 +0800

    解决有的eclipse版本无法获取velocity模板的问题

--- bundles/com.tulin.v8.ureport.server/src/com/tulin/v8/ureport/server/Activator.java
+++ bundles/com.tulin.v8.ureport.server/src/com/tulin/v8/ureport/server/Activator.java
@@ -9,25 +9,27 @@ public class Activator extends AbstractUIPlugin {
 	public static final String PLUGIN_ID = "com.tulin.v8.ureport.server";
 	private static Activator plugin;
 	public static ApplicationContext applicationContext;
+	private final ClassLoader oldContextClassLoader = Thread.currentThread().getContextClassLoader();
 
-	public Activator() {
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		// TODO:解决无法获取 velocity 模板的问题
+		Thread.currentThread().setContextClassLoader(Activator.class.getClassLoader());
 		try {
 			applicationContext = new ClassPathXmlApplicationContext("ureport-console-context.xml");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-	
+
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
+		// set back default class loader
+		Thread.currentThread().setContextClassLoader(oldContextClassLoader);
 		super.stop(context);
 	}
-	
+
 	public static Activator getDefault() {
 		return plugin;
 	}
