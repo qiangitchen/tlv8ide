12ab336398f767c9de10e3b35b3f760e637e81c1
陈乾 <qianpou@163.com>
2022-08-08 17:44:29 +0800

    忽略echarts预览时数据库未连接异常

--- bundles/com.tulin.v8.editors.page.server/src/com/tulin/v8/editors/page/server/echat/DataBaseSQLParse.java
+++ bundles/com.tulin.v8.editors.page.server/src/com/tulin/v8/editors/page/server/echat/DataBaseSQLParse.java
@@ -15,8 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.naming.NamingException;
-
 import org.dom4j.Element;
 
 import com.tulin.v8.core.DBUtils;
@@ -51,91 +49,96 @@ public class DataBaseSQLParse {
 		}
 	}
 
-	public static List execQueryforList(String key, String sql, Parameters parameter) throws SQLException, NamingException {
-		ResultSet rs = null;
-		ResultSetMetaData rsmd = null;
+	public static List execQueryforList(String key, String sql, Parameters parameter) {
 		List li = new ArrayList();
-		Connection aConn = DBUtils.getAppConn(key);
-		List<String> paramlist = new ArrayList<String>();
-		Map<String, String> parameters = new HashMap<String, String>();
-		if (parameter.getParamsMap().containsKey(sql)) {
-			parameters = parameter.getParamsMap().get(sql);
-			for (String param : parameters.keySet()) {
-				paramlist.add(param);
-				sql = sql.replace("{" + param + "}", "?");
-			}
-		}
-		PreparedStatement ps = aConn.prepareStatement(sql);
 		try {
-			for (int p = 0; p < paramlist.size(); p++) {
-				String pam = paramlist.get(p);
-				String pav = parameters.get(pam);
-				ps.setString(p + 1, pav);
+			ResultSet rs = null;
+			ResultSetMetaData rsmd = null;
+			Connection aConn = DBUtils.getAppConn(key);
+			List<String> paramlist = new ArrayList<String>();
+			Map<String, String> parameters = new HashMap<String, String>();
+			if (parameter.getParamsMap().containsKey(sql)) {
+				parameters = parameter.getParamsMap().get(sql);
+				for (String param : parameters.keySet()) {
+					paramlist.add(param);
+					sql = sql.replace("{" + param + "}", "?");
+				}
 			}
-			rs = ps.executeQuery();
-			rsmd = rs.getMetaData();
-			int size = rsmd.getColumnCount();
-			while (rs.next()) {
-				Map sm = new HashMap();
-				for (int i = 1; i <= size; i++) {
-					String cellType = rsmd.getColumnTypeName(i);
-					String columnName = rsmd.getColumnLabel(i);
-					if ("DATE".equals(cellType) || "DATETIME".equals(cellType)) {
-						try {
+			PreparedStatement ps = aConn.prepareStatement(sql);
+			try {
+				for (int p = 0; p < paramlist.size(); p++) {
+					String pam = paramlist.get(p);
+					String pav = parameters.get(pam);
+					ps.setString(p + 1, pav);
+				}
+				rs = ps.executeQuery();
+				rsmd = rs.getMetaData();
+				int size = rsmd.getColumnCount();
+				while (rs.next()) {
+					Map sm = new HashMap();
+					for (int i = 1; i <= size; i++) {
+						String cellType = rsmd.getColumnTypeName(i);
+						String columnName = rsmd.getColumnLabel(i);
+						if ("DATE".equals(cellType) || "DATETIME".equals(cellType)) {
 							try {
-								SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-								String v_l = format.format(format.parse(rs.getString(i)));
-								sm.put(columnName, v_l);
-							} catch (Exception e) {
 								try {
-									SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-									SimpleDateFormat nformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-									String v_l = nformat.format(format.parse(rs.getString(i)));
-									sm.put(columnName, v_l);
-								} catch (Exception er) {
-									SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+									SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 									String v_l = format.format(format.parse(rs.getString(i)));
 									sm.put(columnName, v_l);
+								} catch (Exception e) {
+									try {
+										SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+										SimpleDateFormat nformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+										String v_l = nformat.format(format.parse(rs.getString(i)));
+										sm.put(columnName, v_l);
+									} catch (Exception er) {
+										SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+										String v_l = format.format(format.parse(rs.getString(i)));
+										sm.put(columnName, v_l);
+									}
 								}
+							} catch (Exception e) {
+								sm.put(columnName, "");
 							}
-						} catch (Exception e) {
-							sm.put(columnName, "");
-						}
-					} else if ("BLOB".equals(cellType)) {
-						try {
-							sm.put(columnName, rs.getBlob(i));
-						} catch (Exception e) {
-							sm.put(columnName, null);
-							Sys.printMsg(e.getMessage());
-						}
-					} else if ("CLOB".equals(cellType)) {
-						Clob clob = rs.getClob(i);
-						String content = "";
-						if (clob != null) {
-							BufferedReader in = new BufferedReader(clob.getCharacterStream());
-							StringWriter out = new StringWriter();
-							int c;
+						} else if ("BLOB".equals(cellType)) {
 							try {
-								while ((c = in.read()) != -1) {
-									out.write(c);
-								}
-							} catch (IOException e) {
-								e.printStackTrace();
+								sm.put(columnName, rs.getBlob(i));
+							} catch (Exception e) {
+								sm.put(columnName, null);
+								Sys.printMsg(e.getMessage());
 							}
-							content = out.toString();
+						} else if ("CLOB".equals(cellType)) {
+							Clob clob = rs.getClob(i);
+							String content = "";
+							if (clob != null) {
+								BufferedReader in = new BufferedReader(clob.getCharacterStream());
+								StringWriter out = new StringWriter();
+								int c;
+								try {
+									while ((c = in.read()) != -1) {
+										out.write(c);
+									}
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+								content = out.toString();
+							}
+							sm.put(columnName, content);
+						} else {
+							sm.put(columnName, (rs.getString(i) == null) ? "" : String.valueOf(rs.getString(i)));
 						}
-						sm.put(columnName, content);
-					} else {
-						sm.put(columnName, (rs.getString(i) == null) ? "" : String.valueOf(rs.getString(i)));
 					}
+					li.add(sm);
 				}
-				li.add(sm);
+			} catch (SQLException e) {
+				Sys.printMsg(e.getMessage());
+				throw new SQLException(e.getMessage() + ">>\n sql:" + sql);
+			} finally {
+				DBUtils.CloseConn(aConn, ps, rs);
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			Sys.printMsg(e.getMessage());
-			throw new SQLException(e.getMessage() + ">>\n sql:" + sql);
-		} finally {
-			DBUtils.CloseConn(aConn, ps, rs);
+			e.printStackTrace();
 		}
 		return li;
 	}
