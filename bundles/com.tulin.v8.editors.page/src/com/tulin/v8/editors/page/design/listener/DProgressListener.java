18b5d4d3454f8b16a691a2545163457a7c041bbb
陈乾 <qianpou@163.com>
2022-08-10 12:28:44 +0800

    修改页面编辑器：有的浏览器执行脚本异常的问题。

--- bundles/com.tulin.v8.editors.page/src/com/tulin/v8/editors/page/design/listener/DProgressListener.java
+++ bundles/com.tulin.v8.editors.page/src/com/tulin/v8/editors/page/design/listener/DProgressListener.java
@@ -28,13 +28,12 @@ public class DProgressListener implements ProgressListener {
 		try {
 			Browser browser = designeditor.getBrowser();
 			if (browser != null) {
-				// browser.execute("testFn('测试信息')");
-				String text = "window.document.onclick = function(event){var obj = event.target||event.srcElement; window.document.title = \"{\\\"boj\\\":\\\"\"+obj+\"\\\",\\\"id\\\":\\\"\"+obj.id+\"\\\"}\";}return;";
-				browser.evaluate(text);
-				browser.evaluate("$(document.body).attr('contenteditable',true);return;");
+				String text = "window.document.onclick = function(event){var obj = event.target||event.srcElement; window.document.title = \"{'boj':'\"+obj+\"','id':'\"+obj.id+\"'}\";};";
+				browser.execute(text);
+				browser.execute("$(document.body).attr('contenteditable',true);");
 			}
 		} catch (Exception e) {
-			// e.printStackTrace();
+			e.printStackTrace();
 		}
 
 	}
