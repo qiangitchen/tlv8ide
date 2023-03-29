7354ecde4ce32abf7f4bac0a5cdf7f8801969930
陈乾 <qianpou@163.com>
2023-03-29 18:01:47 +0800

    增加vue文件编辑器

---
+++ bundles/com.tulin.v8.editors.vue/src/com/tulin/v8/editors/vue/editor/VueEditor.java
@@ -0,0 +1,38 @@
+package com.tulin.v8.editors.vue.editor;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.wst.sse.ui.StructuredTextEditor;
+
+public class VueEditor extends StructuredTextEditor {
+	public VueEditor() {
+		super();
+	}
+
+	public boolean isFileEditorInput() {
+		if (getEditorInput() instanceof IFileEditorInput) {
+			return true;
+		}
+		return false;
+	}
+
+	public File getTempFile() {
+		IFile file = ((FileEditorInput) this.getEditorInput()).getFile();
+		return new File(file.getLocation().makeAbsolute().toFile().getParentFile(), "." + file.getName());
+	}
+
+	public File getFile() {
+		if (this.getEditorInput() instanceof FileEditorInput) {
+			FileEditorInput fileeditorinput = (FileEditorInput) this.getEditorInput();
+			IFile file = fileeditorinput.getFile();
+			return file.getLocation().makeAbsolute().toFile();
+		} else {
+			File fle = new File(this.getEditorInput().getToolTipText());
+			return fle;
+		}
+
+	}
+}
