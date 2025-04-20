fc9eb316ce04c273cbf99ff686029f749084ead0
陈乾 <qianpou@163.com>
2025-04-20 09:13:03 +0800

    Chromium插件配置

---
+++ com.tulin.v8.swt.chromium.bundles/com.tulin.v8.swt.chromium/src/com/tulin/v8/Activator.java
@@ -0,0 +1,52 @@
+package com.tulin.v8;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+import com.tulin.v8.cef.CefBrowserManager;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.tulin.v8.swt.chromium"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		try {
+			CefBrowserManager.init();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+		CefBrowserManager.release();
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
