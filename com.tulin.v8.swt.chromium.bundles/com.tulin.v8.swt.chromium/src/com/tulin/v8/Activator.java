bbc3f6243614b5c1843b790aa87d3a983a7a3214
陈乾 <qianpou@163.com>
2025-04-22 22:37:01 +0800

    组件初始化

--- com.tulin.v8.swt.chromium.bundles/com.tulin.v8.swt.chromium/src/com/tulin/v8/Activator.java
+++ com.tulin.v8.swt.chromium.bundles/com.tulin.v8.swt.chromium/src/com/tulin/v8/Activator.java
@@ -26,6 +26,10 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		try {
+			CefBrowserManager.init();
+		} catch (Exception | Error e) {
+		}
 	}
 
 	@Override
