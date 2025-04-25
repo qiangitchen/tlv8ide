4e6b70b17863715deb9646d7d406e0da8c774d04
陈乾 <qianpou@163.com>
2025-04-25 18:17:41 +0800

    jcef兼容处理

--- com.tulin.v8.swt.chromium.bundles/com.tulin.v8.swt.chromium/src/com/tulin/v8/cef/CefBrowserManager.java
+++ com.tulin.v8.swt.chromium.bundles/com.tulin.v8.swt.chromium/src/com/tulin/v8/cef/CefBrowserManager.java
@@ -37,7 +37,8 @@ public class CefBrowserManager {
 
 	public static void init() throws Exception {
 		// && OSSelect.isARM() && OSSelect.osVersion() <= 5.19
-		if (OSSelect.isLinux() && OSSelect.getGCCVersion() < 9.0) {
+//		if (OSSelect.isLinux() && OSSelect.getGCCVersion() < 9.0) {
+		if (!OSSelect.isWindows()) {
 			throw new Exception("系统不支持~");
 		}
 		if (cefApp == null) {
